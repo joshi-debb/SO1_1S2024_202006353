e7dfc042bf0c58a606fa26e024ccb9c758eac217
Josue Zuleta <mym.jayjay@gmail.com>
2024-02-09 19:12:56 -0600

    tarea 2 modo f :(

---
+++ Tareas/Tarea2/backend/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+const PORT = 2024;
+
+app.use(cors());
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
+app.use(morgan('dev'));
+app.use(express.json()); 
+
+mongoose.connect('mongodb://DB:27017/DB', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error de conexión a MongoDB:'));
+db.once('open', () => {
+  console.log('Conexión exitosa a MongoDB');
+});
+
+const Persona = mongoose.model('personas', {
+  foto: String,
+  fecha: Date
+});
+
+
+// Insertar un nuevo persona
+app.post('/InsertPersona', async (req, res) => {
+  const { foto, fecha} = req.body;
+
+  try {
+    const nuevoPersona = new Persona({ foto, fecha });
+    await nuevoPersona.save();
+    res.status(201).json(nuevoPersona);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al insertar el Persona' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor API en http://localhost:${PORT}`);
+});
---
+++ Tareas/Tarea2/frontend/src/App.js
@@ -0,0 +1,51 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  async function takeSnapshot() {
+    const webcamElement = document.getElementById("webcam");
+    const canvasElement = document.createElement("canvas");
+    canvasElement.width = webcamElement.videoWidth;
+    canvasElement.height = webcamElement.videoHeight;
+    const canvas = canvasElement.getContext("2d");
+    canvas.drawImage(webcamElement, 0, 0, webcamElement.videoWidth, webcamElement.videoHeight);
+    const foto = canvasElement.toDataURL("image/png");
+    const fecha = new Date();
+    const response = await fetch("http://localhost:2024/InsertPersona", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ foto, fecha }),
+    });
+    const data = await response.json();
+    console.log(data);
+    
+  }
+
+
+    async function startWebcam() {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        const webcamElement = document.getElementById("webcam");
+        webcamElement.srcObject = stream;
+      } catch (error) {
+        console.error("Error accediendo a la  webcam:", error);
+      }
+    }
+  startWebcam();
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <video id="webcam" autoPlay playsInline muted></video>
+
+
+        <button id="boton" onClick={takeSnapshot}>Tomar foto</button>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;

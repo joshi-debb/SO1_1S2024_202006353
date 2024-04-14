03dea372e021c29e071c833faf15bf15c3786d43
Juan Josue Zuleta Beb <mym.jayjay@gmail.com>
2024-04-14 05:56:58 -0600

    tarea4

---
+++ Tareas/Tarea4/cliente/cliente.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	pb "cliente/proto" // nombre_proyecto/carpeta
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var ctx = context.Background()
+
+type Data struct {
+	Name  string
+	Album string
+	Year  string
+	Rank  string
+}
+
+func insertData(c *fiber.Ctx) error {
+	var data map[string]string
+	e := c.BodyParser(&data)
+	if e != nil {
+		return e
+	}
+
+	band := Data{
+		Name:  data["name"],
+		Album: data["album"],
+		Year:  data["year"],
+		Rank:  data["rank"],
+	}
+
+	go sendServer(band)
+	return nil
+}
+
+func sendServer(band Data) {
+	conn, err := grpc.Dial("localhost:3001", grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock())
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	cl := pb.NewGetInfoClient(conn)
+	defer func(conn *grpc.ClientConn) {
+		err := conn.Close()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}(conn)
+
+	ret, err := cl.ReturnInfo(ctx, &pb.RequestId{
+		Name:  band.Name,
+		Album: band.Album,
+		Year:  band.Year,
+		Rank:  band.Rank,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("Respuesta del server " + ret.GetInfo())
+}
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"res": "todo bien",
+		})
+	})
+	app.Post("/insert", insertData)
+
+	err := app.Listen(":3000")
+	if err != nil {
+		return
+	}
+}
---
+++ Tareas/Tarea4/servidor/servidor.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"net"
+	pb "serverGRPC/proto"
+
+	_ "github.com/go-sql-driver/mysql"
+	"google.golang.org/grpc"
+)
+
+var ctx = context.Background()
+var db *sql.DB
+
+type server struct {
+	pb.UnimplementedGetInfoServer
+}
+
+const (
+	port = ":3001"
+)
+
+type Data struct {
+	Name    string
+	Album	string
+	Year 	string
+	Rank  	string
+}
+
+func mysqlConnect() {
+	// Cambia las credenciales según tu configuración de MySQL
+	dsn := "root:secret@tcp(localhost:3306)/clase9"
+
+	var err error
+	db, err = sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("Conexión a MySQL exitosa")
+}
+
+func (s *server) ReturnInfo(ctx context.Context, in *pb.RequestId) (*pb.ReplyInfo, error) {
+	fmt.Println("Recibí de cliente: ", in.GetName())
+	data := Data{
+		Name:  in.GetName(),
+		Album: in.getAlbum(),
+		Year:  in.GetYear(),
+		Rank:  in.GetRank(),
+	}
+	fmt.Println(data)
+	insertMySQL(data)
+	return &pb.ReplyInfo{Info: "Hola cliente, recibí el comentario"}, nil
+}
+
+func insertMySQL(band Data) {
+	// Prepara la consulta SQL para la inserción en MySQL
+	query := "INSERT INTO band (name, album, year, rank) VALUES (?, ?, ?, ?)"
+	_, err := db.ExecContext(ctx, query, band.name, band.album, band.year, band.rank)
+	if err != nil {
+		log.Println("Error al insertar en MySQL:", err)
+	}
+}
+
+func main() {
+	listen, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGetInfoServer(s, &server{})
+
+	mysqlConnect()
+
+	if err := s.Serve(listen); err != nil {
+		log.Fatalln(err)
+	}
+}
